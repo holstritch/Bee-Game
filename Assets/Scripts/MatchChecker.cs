b8f3de392cb9eec03ed3e21c549222d6e3fbf721
ebgames <84336806+ebgames@users.noreply.github.com>
2022-11-08 20:09:42 +0000

    More test matches

--- Assets/Scripts/MatchChecker.cs
+++ Assets/Scripts/MatchChecker.cs
@@ -8,34 +8,26 @@ public class MatchChecker : MonoBehaviour
     public ProfileChanger profileChanger2;
     public int bee1;
     public int bee2;
-
-    private void Update() // Check which bee is in each slot
-    
-    {
-        bee1 = profileChanger1.currentProfile;  
-        bee2 = profileChanger2.currentProfile;  
-    }
     
     public void CheckForMatch() 
     {
-        if(bee1 == 0 && bee2 == 1)
+        // Check which bee is in each slot
+        bee1 = profileChanger1.currentProfile;  
+        bee2 = profileChanger2.currentProfile; 
+        
+        // Check if the bees are a match or not
+
+        if(bee1 == bee2)
         {
-            Debug.Log("They'll love buzzing about together.");
+            Debug.Log("It's a match!");
+            // Remove these bees from the array
+            // Add win sound effect
         }
 
-        if(bee1 == 1 && bee2 == 2)
-        {
-            Debug.Log("They'll bee happy travelling together!");
-        }
-
-        if(bee1 == 2 && bee2 == 0)
-        {
-            Debug.Log("Lots of laughs for these bees!");
-        }
-
-        else
+        if(bee1 != bee2)
         {
             Debug.Log("These two aren't a match.");
+            // Add incorrect sound effect
         }
         
     }
