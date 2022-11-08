1357e57679a0a9c64797416225e0105eee890b0b
ebgames <84336806+ebgames@users.noreply.github.com>
2022-11-08 19:45:56 +0000

    Test bee matches set up

--- Assets/Scripts/MatchChecker.cs
+++ Assets/Scripts/MatchChecker.cs
@@ -9,23 +9,33 @@ public class MatchChecker : MonoBehaviour
     public int bee1;
     public int bee2;
 
-    private void Update() 
+    private void Update() // Check which bee is in each slot
     
     {
         bee1 = profileChanger1.currentProfile;  
         bee2 = profileChanger2.currentProfile;  
     }
     
-    public void CheckForMatch()
+    public void CheckForMatch() 
     {
-        if(bee1 == bee2)
+        if(bee1 == 0 && bee2 == 1)
         {
-            Debug.Log("It's a match!");
+            Debug.Log("They'll love buzzing about together.");
         }
 
-        if(bee1 != bee2)
+        if(bee1 == 1 && bee2 == 2)
         {
-            Debug.Log("They'd be terrible together!");
+            Debug.Log("They'll bee happy travelling together!");
+        }
+
+        if(bee1 == 2 && bee2 == 0)
+        {
+            Debug.Log("Lots of laughs for these bees!");
+        }
+
+        else
+        {
+            Debug.Log("These two aren't a match.");
         }
         
     }
